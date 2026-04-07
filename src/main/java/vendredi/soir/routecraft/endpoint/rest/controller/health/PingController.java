d27ba6764335613ef4a47168af00584a5c6df5c6
Daniel Langio <langio.tehiniavo@gmail.com>
2026-04-07 17:23:08 +0300

    style: format

--- src/main/java/vendredi/soir/routecraft/endpoint/rest/controller/health/PingController.java
+++ src/main/java/vendredi/soir/routecraft/endpoint/rest/controller/health/PingController.java
@@ -6,8 +6,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import vendredi.soir.routecraft.PojaGenerated;
-import vendredi.soir.routecraft.repository.DummyRepository;
-import vendredi.soir.routecraft.repository.DummyUuidRepository;
 
 @PojaGenerated
 @RestController
