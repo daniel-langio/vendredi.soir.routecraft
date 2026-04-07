fa34ef75b8d791b69da5636216e614506767b20b
Daniel Langio <langio.tehiniavo@gmail.com>
2026-04-07 17:20:45 +0300

    init

--- src/main/java/vendredi/soir/routecraft/endpoint/rest/controller/health/PingController.java
+++ src/main/java/vendredi/soir/routecraft/endpoint/rest/controller/health/PingController.java
@@ -14,9 +14,6 @@ import vendredi.soir.routecraft.repository.DummyUuidRepository;
 @AllArgsConstructor
 public class PingController {
 
-  DummyRepository dummyRepository;
-  DummyUuidRepository dummyUuidRepository;
-
   public static final ResponseEntity<String> OK = new ResponseEntity<>("OK", HttpStatus.OK);
   public static final ResponseEntity<String> KO =
       new ResponseEntity<>("KO", HttpStatus.INTERNAL_SERVER_ERROR);
