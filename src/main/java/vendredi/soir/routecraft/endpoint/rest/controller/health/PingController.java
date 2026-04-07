a06163897871a812c2f89b2dd89346a16ca2bcba
Daniel Langio <langio.tehiniavo@gmail.com>
2026-04-07 18:21:09 +0300

    chore: ping pong with gif

--- src/main/java/vendredi/soir/routecraft/endpoint/rest/controller/health/PingController.java
+++ src/main/java/vendredi/soir/routecraft/endpoint/rest/controller/health/PingController.java
@@ -3,12 +3,12 @@ package vendredi.soir.routecraft.endpoint.rest.controller.health;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 import vendredi.soir.routecraft.PojaGenerated;
 
 @PojaGenerated
-@RestController
+@Controller
 @AllArgsConstructor
 public class PingController {
 
